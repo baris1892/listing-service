67ee0ca22f1db4b3fe641f6ad879ab482b9ee9d2
Baris Top <baris1892@users.noreply.github.com>
2025-10-18 12:32:18 +0200

    added missing swagger ui annotations for AdminListingController

--- src/main/java/dev/baristop/portfolio/listingservice/listing/controller/AdminListingController.java
+++ src/main/java/dev/baristop/portfolio/listingservice/listing/controller/AdminListingController.java
@@ -5,6 +5,8 @@ import dev.baristop.portfolio.listingservice.listing.dto.ListingStatusResponse;
 import dev.baristop.portfolio.listingservice.listing.entity.ListingStatus;
 import dev.baristop.portfolio.listingservice.listing.service.ListingService;
 import dev.baristop.portfolio.listingservice.security.util.Role;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.annotation.Secured;
@@ -22,12 +24,20 @@ public class AdminListingController {
     private final ListingService listingService;
 
     @PatchMapping("/{id}/approve")
+    @Operation(
+        summary = "Approves a listing",
+        security = {@SecurityRequirement(name = "bearerAuth")}
+    )
     public ResponseEntity<ListingStatusResponse> approveListing(@PathVariable Long id) {
         ListingDto updated = listingService.updateListingStatus(id, ListingStatus.APPROVED);
 
         return ResponseEntity.ok(new ListingStatusResponse(updated.getStatus()));
     }
 
+    @Operation(
+        summary = "Rejects a listing",
+        security = {@SecurityRequirement(name = "bearerAuth")}
+    )
     @PatchMapping("/{id}/reject")
     public ResponseEntity<ListingStatusResponse> rejectListing(@PathVariable Long id) {
         ListingDto updated = listingService.updateListingStatus(id, ListingStatus.REJECTED);
